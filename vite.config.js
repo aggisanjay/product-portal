76bca27c0a39527da6b5c79b54582c1ce28d1bf4
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-03-07 18:44:20 +0530

    Fix vite import issue

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-bootstrap']
-    }
-  },
-  resolve: {
-    alias: {
-      'react-bootstrap': 'react-bootstrap/dist/react-bootstrap.min.js'
-    }
-  }
+  
 })
 
